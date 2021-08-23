78507a66e18f78ec9b9196f7b1edcee24b763004
RickDW <rickxdewolf@gmail.com>
2021-08-23 22:34:26 +0000

    WIP

--- code/curriculum_policy/curriculum_env.py
+++ code/curriculum_policy/curriculum_env.py
@@ -1,30 +1,44 @@
 import gym
 from ray.rllib.agents.registry import get_trainer_class
 
-from typing import Union
+from typing import Union, Optional
 from ray.rllib.utils.typing import TrainerConfigDict
 from ray.rllib.agents.trainer import Trainer
 
 
 # TODO
 class CurriculumEnv(gym.Env):
-    def __init__(self, trainer: Union[str, Trainer], trainer_config: TrainerConfigDict):
+    def __init__(
+            self,
+            trainer: Union[str, Trainer],
+            curriculum_config: dict,
+            trainer_config: TrainerConfigDict):
+        
         super().__init__()
 
         if isinstance(trainer, Trainer):
-            self.trainer = trainer(trainer_config)
-        
+            self.trainer = trainer
         elif isinstance(trainer, str):
-            self.trainer = get_trainer_class(trainer)(trainer_config)
-
+            self.trainer = get_trainer_class(trainer)
         else:
             raise ValueError("trainer must be a string or a Trainer class " +
                 f"instead of {type(trainer)}")
 
-    def reset():
+        self.trainer_instance: Optional[Trainer] = None
+        self.trainer_config = trainer_config
+        # TODO: the curriculum config should cover a number of things:
+        #   - the state representation of the learning agent
+        #   - the curriculum agent's rewards
+        #   - the curriculum action space / the set of tasks that can be chosen
+        self.curriculum_config = curriculum_config
+
+    def init_trainer(self):
+        self.trainer_instance = self.trainer(self.trainer_config)
+
+    def reset(self):
         # (re)initialize the learning agent / it's policy, and return its
         # current state as an observation of the curriculum agent
-        pass
+        self.init_trainer()
 
     def step(action):
         # set up the next task environment for the learning agent based on the
@@ -33,6 +47,7 @@ class CurriculumEnv(gym.Env):
         pass
 
 
+# TODO
 class LearningEnv(gym.Env):
     def __init__(self):
         super().__init__()
